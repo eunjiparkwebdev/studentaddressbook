9a4f1694baff6d3463068b94c2e499526174125e
Eunji Park <eunjipark1984@gmail.com>
2023-03-22 21:59:39 -0400

    data

--- src/component/card/card.component.jsx
+++ src/component/card/card.component.jsx
@@ -1,11 +1,15 @@
 import "./card.styles.css";
 
 const Card = ({ student }) => {
-  const { id, name, email } = student;
+  const { id, name, email, imageUrl } = student;
   return (
     <div className="card-container" key={id}>
       <img
-        src={`http://robohash.org/${id}?set=set4&size=180x180`}
+        src={
+          imageUrl === ""
+            ? `http://robohash.org/${id}?set=set4&size=180x180`
+            : `${imageUrl}`
+        }
         alt={`student ${name}`}
       ></img>
       <h2>{name}</h2>
